b9b1b0edabd462f7200c6d4e61c6539a8ce2c500
rangushivakumar <shivakumarrangu555@gmail.com>
2024-01-11 17:43:02 +0530

    again changed

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import macrosPlugin from "vite-plugin-babel-macros";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),macrosPlugin()],
+  plugins: [react()],
+  base:"/project/"
 })
